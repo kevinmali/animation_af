c29e01c28378ebcf240d48b05f331dbcd1d1e454
r33 <kevinmali0433@gmail.com>
2023-09-06 11:03:52 +0530

    ui

--- lib/Views/Screen/Home_page.dart
+++ lib/Views/Screen/Home_page.dart
@@ -17,55 +17,217 @@ class _HomeState extends State<Home> {
         children: [
           Visibility(
             visible: Droop == false,
-            child: Draggable(
-              data: 'lion',
-              childWhenDragging: Container(
-                height: 200,
-                width: 200,
-              ),
-              feedback: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("Asset/images/images1.jpeg"),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Draggable(
+                    data: 'lion',
+                    childWhenDragging: Container(
+                      height: 200,
+                      width: 200,
+                    ),
+                    feedback: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images1.jpeg"),
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images1.jpeg"),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("Asset/images/images1.jpeg"),
+                  Draggable(
+                    data: 'Elephant',
+                    childWhenDragging: Container(
+                      height: 200,
+                      width: 200,
+                    ),
+                    feedback: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images2.jpeg"),
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images1.jpeg"),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Draggable(
+                    data: 'lion',
+                    childWhenDragging: Container(
+                      height: 200,
+                      width: 200,
+                    ),
+                    feedback: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images1.jpeg"),
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images1.jpeg"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Draggable(
+                    data: 'lion',
+                    childWhenDragging: Container(
+                      height: 200,
+                      width: 200,
+                    ),
+                    feedback: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images1.jpeg"),
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Asset/images/images1.jpeg"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          DragTarget(
-            onWillAccept: (val) {
-              return val == 'lion';
-            },
-            onAccept: (val) {
-              setState(() {
-                Droop = true;
-              });
-            },
-            builder: (BuildContext ctx, List<Object?> accepted,
-                List<dynamic> rejectedData) {
-              return Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: (Droop == true)
-                        ? AssetImage("Asset/images/imageslioncage.jpeg")
-                        : AssetImage("Asset/images/imagescage.jpeg"),
-                  ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            child: Row(
+              children: [
+                DragTarget(
+                  onWillAccept: (val) {
+                    return val == 'lion';
+                  },
+                  onAccept: (val) {
+                    setState(() {
+                      Droop = true;
+                    });
+                  },
+                  builder: (BuildContext ctx, List<Object?> accepted,
+                      List<dynamic> rejectedData) {
+                    return Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: (Droop == true)
+                              ? AssetImage("Asset/images/imageslioncage.jpeg")
+                              : AssetImage("Asset/images/imagescage.jpeg"),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+                DragTarget(
+                  onWillAccept: (val) {
+                    return val == 'Elephant';
+                  },
+                  onAccept: (val) {
+                    setState(() {
+                      Droop = true;
+                    });
+                  },
+                  builder: (BuildContext ctx, List<Object?> accepted,
+                      List<dynamic> rejectedData) {
+                    return Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: (Droop == true)
+                              ? AssetImage("Asset/images/imageslioncage.jpeg")
+                              : AssetImage("Asset/images/imagescage.jpeg"),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                DragTarget(
+                  onWillAccept: (val) {
+                    return val == 'lion';
+                  },
+                  onAccept: (val) {
+                    setState(() {
+                      Droop = true;
+                    });
+                  },
+                  builder: (BuildContext ctx, List<Object?> accepted,
+                      List<dynamic> rejectedData) {
+                    return Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: (Droop == true)
+                              ? AssetImage("Asset/images/imageslioncage.jpeg")
+                              : AssetImage("Asset/images/imagescage.jpeg"),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                DragTarget(
+                  onWillAccept: (val) {
+                    return val == 'lion';
+                  },
+                  onAccept: (val) {
+                    setState(() {
+                      Droop = true;
+                    });
+                  },
+                  builder: (BuildContext ctx, List<Object?> accepted,
+                      List<dynamic> rejectedData) {
+                    return Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: (Droop == true)
+                              ? AssetImage("Asset/images/imageslioncage.jpeg")
+                              : AssetImage("Asset/images/imagescage.jpeg"),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
